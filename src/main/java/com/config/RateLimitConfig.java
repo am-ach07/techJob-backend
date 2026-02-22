301ee851fa404c020991ec45e061166af457d439
am-ach07 <amarabdelmounaim@gmail.com>
2026-02-22 11:18:46 +0100

    deploy

--- src/main/java/com/config/RateLimitConfig.java
+++ src/main/java/com/config/RateLimitConfig.java
@@ -15,42 +15,42 @@ import io.github.bucket4j.distributed.BucketProxy;
 import io.github.bucket4j.distributed.proxy.ProxyManager;
 import jakarta.servlet.http.HttpServletRequest;
 
-@Configuration
-public class RateLimitConfig {
-
-    private final ProxyManager<String> proxyManager;
-
-    public RateLimitConfig(ProxyManager<String> proxyManager) {
-        this.proxyManager = proxyManager;
-    }
-
-    @Bean
-    public Function<HttpServletRequest, BucketProxy> rateLimitResolver() {
-        return request -> {
-
-        	String ip = request.getRemoteAddr();
-        	String endpoint = request.getRequestURI()
-        	        .replaceAll("[^a-zA-Z0-9]", "_");
-
-        	String key = "rate_limit:" + endpoint + ":" + ip;
-        	System.out.println("keys"+key);
-            Bandwidth limit;
-
-            if (request.getRequestURI().startsWith(SecurityConstants.AUTH_API_PATH)) {
-                limit = Bandwidth.classic(5,
-                        Refill.intervally(5, Duration.ofMinutes(1)));
-            } else {
-                limit = Bandwidth.classic(20,
-                        Refill.intervally(20, Duration.ofMinutes(1)));
-            }
-
-            BucketConfiguration configuration =
-                    BucketConfiguration.builder()
-                            .addLimit(limit)
-                            .build();
-
-            return proxyManager.builder()
-                    .build(key, configuration);
-        };
-    }
-}
+//@Configuration
+//public class RateLimitConfig {
+//
+//    private final ProxyManager<String> proxyManager;
+//
+//    public RateLimitConfig(ProxyManager<String> proxyManager) {
+//        this.proxyManager = proxyManager;
+//    }
+//
+//    @Bean
+//    public Function<HttpServletRequest, BucketProxy> rateLimitResolver() {
+//        return request -> {
+//
+//        	String ip = request.getRemoteAddr();
+//        	String endpoint = request.getRequestURI()
+//        	        .replaceAll("[^a-zA-Z0-9]", "_");
+//
+//        	String key = "rate_limit:" + endpoint + ":" + ip;
+//        	System.out.println("keys"+key);
+//            Bandwidth limit;
+//
+//            if (request.getRequestURI().startsWith(SecurityConstants.AUTH_API_PATH)) {
+//                limit = Bandwidth.classic(5,
+//                        Refill.intervally(5, Duration.ofMinutes(1)));
+//            } else {
+//                limit = Bandwidth.classic(20,
+//                        Refill.intervally(20, Duration.ofMinutes(1)));
+//            }
+//
+//            BucketConfiguration configuration =
+//                    BucketConfiguration.builder()
+//                            .addLimit(limit)
+//                            .build();
+//
+//            return proxyManager.builder()
+//                    .build(key, configuration);
+//        };
+//    }
+//}
--- src/main/java/com/config/RedisBucketConfig.java
+++ src/main/java/com/config/RedisBucketConfig.java
@@ -12,35 +12,35 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class RedisBucketConfig {
-
-    @Value("${spring.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.redis.port}")
-    private int redisPort;
-
-    @Bean
-    public RedisClient redisClient() {
-        String redisUrl = "redis://" + redisHost + ":" + redisPort;
-        return RedisClient.create(redisUrl);
-    }
-
-    @Bean
-    public StatefulRedisConnection<String, byte[]> redisConnection(RedisClient redisClient) {
-
-        RedisCodec<String, byte[]> codec =
-                RedisCodec.of(new StringCodec(), new ByteArrayCodec());
-
-        return redisClient.connect(codec);
-    }
-
-    @Bean
-    public ProxyManager<String> proxyManager(
-            StatefulRedisConnection<String, byte[]> connection) {
-
-        return LettuceBasedProxyManager.builderFor(connection)
-                .build();
-    }
-}
+//@Configuration
+//public class RedisBucketConfig {
+//
+//    @Value("${spring.redis.host}")
+//    private String redisHost;
+//
+//    @Value("${spring.redis.port}")
+//    private int redisPort;
+//
+//    @Bean
+//    public RedisClient redisClient() {
+//        String redisUrl = "redis://" + redisHost + ":" + redisPort;
+//        return RedisClient.create(redisUrl);
+//    }
+//
+//    @Bean
+//    public StatefulRedisConnection<String, byte[]> redisConnection(RedisClient redisClient) {
+//
+//        RedisCodec<String, byte[]> codec =
+//                RedisCodec.of(new StringCodec(), new ByteArrayCodec());
+//
+//        return redisClient.connect(codec);
+//    }
+//
+//    @Bean
+//    public ProxyManager<String> proxyManager(
+//            StatefulRedisConnection<String, byte[]> connection) {
+//
+//        return LettuceBasedProxyManager.builderFor(connection)
+//                .build();
+//    }
+//}
