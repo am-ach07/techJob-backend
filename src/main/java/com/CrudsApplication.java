1141b63359629e5909dd5c7d3929d6be8b3faee0
am-ach07 <amarabdelmounaim@gmail.com>
2026-02-23 11:03:44 +0100

    deploy

--- src/main/java/com/CrudsApplication.java
+++ src/main/java/com/CrudsApplication.java
@@ -1,11 +1,8 @@
 package com;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import jakarta.annotation.PostConstruct;
 
 
 @EnableScheduling
@@ -16,25 +13,7 @@ public class CrudsApplication {
 		SpringApplication.run(CrudsApplication.class, args);
 	}
 	
-	@Value("${spring.datasource.url}")
-    private String url;
-
-	@Value("${spring.datasource.password}")
-    private String pswd;
-
-    @Value("${spring.datasource.username}")
-    private String user;
-
-    
-
-    @PostConstruct
-    public void printEnv() {
-        System.out.println("=== RAILWAY DB ENV VALUES ===");
-        System.out.println("MYSQL_URL = " + url);
-        System.out.println("MYSQLPASSWORD = " + pswd);
-        System.out.println("MYSQLUSER = " + user);
-        System.out.println("==============================");
-    }
+	
 	
 	
 
